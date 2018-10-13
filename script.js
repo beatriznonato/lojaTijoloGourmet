bc07ab21511fac32916af0551e27c4413eb6f17f
Beatriz <bia_nonato@icloud.com>
2018-10-13 20:52:12 -0300

    inserir funcionalidade JS e configurar menu e barra de pesquisa

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-onst tijolo = document.querySelector('.produto');
+const tijolo = document.querySelector('.produto');
 const botaoMenu = document.querySelector('header div');
 const menuFlutuante = document.querySelector('#navegacao nav');
 const barraDePesquisa = document.querySelector('input');
- function alertar(){
-  alert("Não clique aqui");
+function alertar(){
+  alert("Digite a palavra premiada na pesquisa e ganhe desconto. DICA: nome do site");
 }
- function controlarMenu(){
+
+function controlarMenu(){
   if(menuFlutuante.style.display === "flex"){
     menuFlutuante.style.display = "";
   }
@@ -13,22 +14,26 @@ const barraDePesquisa = document.querySelector('input');
     menuFlutuante.style.display = "flex";
   }
 }
- function pesquisa(event){
+
+function pesquisa(event){
   if(event.key === "Enter"){
     alert("Ainda não funciona a pesquisa, volte outro dia (: ");
     barraDePesquisa.value = "";
   }
 }
- function pintarDeLaranja(){
+
+function pintarDeLaranja(){
   barraDePesquisa.style.backgroundColor = "orange";
 }
- function limparFundo(){
+
+function limparFundo(){
   barraDePesquisa.style.backgroundColor = "";
   if(barraDePesquisa.value.search('tijolo') !== -1){
     alert("PARABENS VOCÊ GANHOU 10% DE DESCONTO NA SUA PRIMEIRA COMPRA");
   }
 }
- botaoMenu.onclick = controlarMenu;
+
+botaoMenu.onclick = controlarMenu;
 tijolo.onclick = alertar;
 barraDePesquisa.onkeypress = pesquisa;
 barraDePesquisa.onkeydown = pintarDeLaranja;
